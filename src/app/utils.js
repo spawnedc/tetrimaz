36d457a3ce2bdb5fe14ddeaf6b3383d52c46b529
Can Yilmaz <spawnedc@gmail.com>
2017-01-12 09:02:50 +0000

    Utility function: logMatrix

--- src/app/utils.js
+++ src/app/utils.js
@@ -2,4 +2,11 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export {randomInt};
+function logMatrix(matrixLikeArray) {
+  matrixLikeArray.forEach((subArr, idx) => {
+    console.log(idx, subArr.join(' '));
+  });
+  console.warn(new Array(100).fill('*').join(''));
+}
+
+export {randomInt, logMatrix};
